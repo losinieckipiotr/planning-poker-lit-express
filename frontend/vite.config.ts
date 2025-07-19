27bdf1555cb3a8d39c70ad7176ae9fc6d7d9fb9c
Piotr Łosiniecki <losnieckipiotr@gmail.com>
2025-07-19 22:17:15 +0200

    use minify-html-literals and fix package versions

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import minifyHTML from 'rollup-plugin-minify-html-literals'
+import { defineConfig } from 'vite'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    // @ts-expect-error Common.JS export does not work with types ?
+    minifyHTML.default(),
   ],
 })
