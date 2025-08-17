abc252cae41925ee6197b24fbab7e0b15337c6b3
Piotr Łosiniecki <losnieckipiotr@gmail.com>
2025-08-17 17:19:31 +0200

    call api in frontend app and scripts fixes

--- src/my-element.ts
+++ src/my-element.ts
@@ -1,5 +1,6 @@
 import { LitElement, html } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
+import type { GetUserReq, GetUserRsp } from '../backend/api/get-user.api'
 import litLogo from './assets/lit.svg'
 import { globalStyles } from './globalStyles'
 import viteLogo from '/vite.svg'
@@ -32,8 +33,43 @@ export class MyElement extends LitElement {
   @property({ type: Number })
   count = 0
 
-  private _onClick() {
+  private async onClick() {
+    await this.fetchUser(this.count)
     this.count++
+  }
+
+  async fetchUser(id: number) {
+    const params: GetUserReq = {
+      data: {
+        id,
+      },
+    }
+
+    const rsp = await fetch('/api/get-user', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(params),
+    })
+
+    const json = (await rsp.json()) as GetUserRsp
+
+    if (json.status == 'ok') {
+      console.log({
+        status: rsp.status,
+        statusText: rsp.statusText,
+        headers: rsp.headers,
+        user: json.data.user,
+      })
+    } else {
+      console.log({
+        status: rsp.status,
+        statusText: rsp.statusText,
+        headers: rsp.headers,
+        error: json.error,
+      })
+    }
   }
 
   render() {
@@ -46,20 +82,28 @@ export class MyElement extends LitElement {
           href="https://vite.dev"
           target="_blank"
         >
-          <img src=${viteLogo} class="h-32" alt="Vite logo" />
+          <img
+            src=${viteLogo}
+            class="h-32"
+            alt="Vite logo"
+          />
         </a>
         <a
           class="block p-10 transition duration-300 hover:drop-shadow-[0_0px_2rem_#325cffaa]"
           href="https://lit.dev"
           target="_blank"
         >
-          <img src=${litLogo} class="lit h-32" alt="Lit logo" />
+          <img
+            src=${litLogo}
+            class="lit h-32"
+            alt="Lit logo"
+          />
         </a>
       </div>
       <slot></slot>
       <button
         class="m-[2rem] cursor-pointer rounded-lg border border-transparent bg-gray-300 px-3 py-2 font-medium transition-[border-color] duration-300 hover:border-[#646cff] dark:bg-stone-900"
-        @click=${this._onClick}
+        @click=${this.onClick}
         part="button"
       >
         count is ${count}
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import minifyHTML from 'rollup-plugin-minify-html-literals'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+    open: '/',
+  },
   plugins: [
     tailwindcss(),
     // @ts-expect-error Common.JS export does not work with types ?
