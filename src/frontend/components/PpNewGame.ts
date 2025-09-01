7e79c2a23255760142ad6af22a08172bca75a164
Piotr Łosiniecki <losnieckipiotr@gmail.com>
2025-09-01 23:12:11 +0200

    css fixers

--- src/frontend/components/PpNewGame.ts
+++ src/frontend/components/PpNewGame.ts
@@ -49,77 +49,73 @@ export class PpNewGame extends LitElement {
     }
   }
 
+  onSubmit(event: SubmitEvent) {
+    event.preventDefault()
+
+    const form = event.currentTarget as HTMLFormElement
+    const formData = Object.fromEntries(new FormData(form))
+    console.log({ formData })
+    // TODO: sent to backend
+
+    this.onCreateRoom()
+  }
+
+  // fieldset bg-base-200 border-base-300 rounded-box w-full border p-4
+
   protected render() {
     return html`
       <div class="grid w-full place-items-center">
-        <div class="card card-xl shadow-xl">
-          <div class="card-body">
-            <h1 class="card-title">New game</h1>
-            <p>To start a new game just enter a game name and let's play!</p>
-            <div class="divider m-0"></div>
-            <form
-              id="star-game-form"
-              class="w-full"
-              @submit=${(event: SubmitEvent) => {
-                event.preventDefault()
-
-                const form = event.currentTarget as HTMLFormElement
-                const formData = Object.fromEntries(new FormData(form))
-                console.log({ formData })
-                // TODO: sent to backend
-
-                this.onCreateRoom()
-              }}
-            >
-              <div class="flex flex-col items-end gap-2">
-                <label class="floating-label mb-4 w-full text-lg">
-                  <h3 class="text-xl">Game name</h3>
-                  <input
-                    name="game-name"
-                    type="text"
-                    class="input input-xl input-primary w-full"
-                    placeholder="Awesome team planning"
-                    list="names"
-                    minlength="3"
-                    maxlength="30"
+        <fieldset
+          class="fieldset bg-base-200 border-base-300 rounded-box w-lg border p-4 text-base"
+        >
+          <legend class="fieldset-legend w-full text-2xl font-bold">
+            New game
+          </legend>
+          <p class="mb-1">
+            To start a new game just enter a game name and let's play!
+          </p>
+          <div class="divider"></div>
+          <label>Game name</label>
+          <input
+            type="text"
+            class="input input-lg input-primary mb-8 w-full"
+            placeholder="Awesome team planning"
+          />
+          <ul
+            class="menu menu-horizontal rounded-box flex w-full flex-row flex-nowrap gap-4 p-0"
+          >
+            <li>
+              <button
+                type="submit"
+                class="btn btn-primary w-30"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"
                   />
-                </label>
-                <ul class="card-actions w-full flex-nowrap">
-                  <li>
-                    <button
-                      type="submit"
-                      class="btn btn-primary btn-lg w-30"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="size-6"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"
-                        />
-                      </svg>
-                      Play
-                    </button>
-                  </li>
-                  <li>
-                    <a
-                      href="/"
-                      class="btn btn-ghost btn-lg w-30"
-                    >
-                      Cancel
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </form>
-          </div>
-        </div>
+                </svg>
+                Play
+              </button>
+            </li>
+            <li>
+              <a
+                href="/"
+                class="btn btn-ghost w-30"
+              >
+                Cancel
+              </a>
+            </li>
+          </ul>
+        </fieldset>
       </div>
     `
   }
