aea7fd1638e7539901b7f9ed0af2755c0fe566c9
Piotr Łosiniecki <losnieckipiotr@gmail.com>
2025-09-01 21:43:59 +0200

    smoll refactor

--- src/frontend/index.ts
+++ src/frontend/index.ts
@@ -1,13 +1,51 @@
-// preload lit
-import '@lit/reactive-element'
-import 'lit-html'
-import { initColorSchemes } from './initColorSchemes.js'
-import { loadPolyfills } from './loadPolyfills.js'
-import { loadStyleSheet } from './loadStyleSheet.js'
+import { unsafeCSS } from 'lit'
+import { DARK_THEME, LIGHT_THEME, THEME_ATTRIBUTE } from './enums.js'
+import type { Theme } from './types.js'
 
-loadPolyfills()
-initColorSchemes()
-await loadStyleSheet()
+startApp()
 
-// lazy load main app component
-import('./components/PpApp.js')
+async function startApp() {
+  await Promise.all([loadStyleSheet(), loadPolyfills()])
+
+  initColorSchemes()
+
+  // lazy load main app component
+  import('./components/PpApp.js')
+}
+
+async function loadStyleSheet() {
+  const link = document.querySelector<HTMLLinkElement>('[rel="stylesheet"]')!
+
+  const rsp = await fetch(link.href, {
+    headers: { accept: 'text/css' },
+    cache: 'force-cache',
+  })
+  const text = await rsp.text()
+
+  const globalStyles = unsafeCSS(text)
+
+  window.globalStyles = globalStyles
+}
+
+async function loadPolyfills() {
+  if (!globalThis.URLPattern) {
+    await import('urlpattern-polyfill')
+  }
+}
+
+function initColorSchemes() {
+  const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)')
+  const savedTheme = localStorage.getItem(THEME_ATTRIBUTE) as Theme | null
+  const initialTheme =
+    savedTheme ? savedTheme
+    : prefersDarkScheme.matches ? DARK_THEME
+    : LIGHT_THEME
+
+  if (!savedTheme) {
+    localStorage.setItem(THEME_ATTRIBUTE, initialTheme)
+  }
+
+  const rootEl = document.querySelector('html')!
+
+  rootEl.setAttribute(THEME_ATTRIBUTE, initialTheme)
+}
--- src/frontend/loadPolyfills.ts
+++
@@ -1,7 +0,0 @@
-export async function loadPolyfills() {
-  // Conditional ESM module loading (Node.js and browser)
-  // @ts-expect-error
-  if (!globalThis.URLPattern) {
-    await import('urlpattern-polyfill')
-  }
-}
